a5c4c9851f5fb2da3f1592c5dc2d08c8c543732b
hal <hal@nutsec.org>
2018-11-15 15:13:46 -0500

    Fixing for newer qemu

--- download.sh
+++ download.sh
@@ -3,7 +3,7 @@
 mkdir qemu-images
 cd qemu-images
 for x in armel armhf mips mipsel; do
-   wget -R '*desktop*,*squeeze*,*2.6*' -A '*initrd*,*vmlinuz*,*.qcow2,*vmlinux*' -r --no-parent https://people.debian.org/~aurel32/qemu/$x/
+   wget -R '*desktop*,*squeeze*' -A '*2.6*,*initrd*,*vmlinuz*,*.qcow2,*vmlinux*' -r --no-parent https://people.debian.org/~aurel32/qemu/$x/
 done
 mv people.debian.org/~aurel32/qemu/* .
 rm -rf people.debian.org
